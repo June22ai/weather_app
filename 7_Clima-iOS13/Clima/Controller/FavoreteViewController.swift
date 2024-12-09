74c3fdd5c5923038019e9e88a309fa5fe47694da
ai.t <strawberrylatte262@gmail.com>
2024-12-09 03:24:36 -0800

    新規画面作成

---
+++ 7_Clima-iOS13/Clima/Controller/FavoreteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoreteViewController.swift
+//  Clima
+//
+//  Created by Ai Tanigwa on 2024/12/09.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class FavoreteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
