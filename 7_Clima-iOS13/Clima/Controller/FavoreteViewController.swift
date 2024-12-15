f0faf46f2b340f83479125f06d857bbbf3620f6d
ai.t <strawberrylatte262@gmail.com>
2024-12-14 19:24:08 -0800

    新しいストーリーボードにViewController追加

--- 7_Clima-iOS13/Clima/Controller/FavoreteViewController.swift
+++ 7_Clima-iOS13/Clima/Controller/FavoreteViewController.swift
@@ -9,22 +9,32 @@
 import UIKit
 
 class FavoreteViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func GoBack(_ sender: Any) {
+        
+        //Storyboardで別のViewControllerに遷移する
+        if let nextViewController = storyboard?.instantiateViewController(withIdentifier: "WeatherViewController") {
+                self.present(nextViewController, animated: true, completion: nil)
+            }
+//        let next = storyboard?.instantiateViewController(withIdentifier: "WeatherViewController") {
+//            self.present(next!, animated: true, completion: nil)
+//
+//        }
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    */
-
 }
