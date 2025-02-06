ec32b939f943b9cc57927be779b63892a1d86178
ai.t <strawberrylatte262@gmail.com>
2025-02-06 05:56:49 -0800

    遷移先にNavigationControllerを埋め込む

--- 7_Clima-iOS13/Clima/Controller/FavoreteViewController.swift
+++ 7_Clima-iOS13/Clima/Controller/FavoreteViewController.swift
@@ -17,23 +17,22 @@ struct Area {
 
 class FavoreteViewController: UIViewController {
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-    
-            self.navigationItem.title = "都市一覧"
-        }
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "都市一覧"
+        
+    }
     //前画面から遷移した時
     @IBOutlet weak var tableView:UITableView!
-    //@IBOutlet var tableView: UITableView!
     {
         didSet {
+            
             tableView.dataSource = self
             tableView.delegate = self
             tableView.tableFooterView = UIView()
         }
     }
-    
+   
     //既存のデータ構造を利用して、アコーディオン用のデータを設定
     private let headerArray: [String] = ["EU", "アジア", "オセアニア", "アフリカ"]
     private let euArray: [String] = ["ベルリン", "アムステルダム", "ロンドン"]
@@ -57,7 +56,7 @@ class FavoreteViewController: UIViewController {
         }
     }
     @IBAction func GoBack(_ sender: UIButton) {
-        //今回はナビゲーションコントローラーは使用していないので↓が呼ばれる事はない
+        
         if let navigationController = self.navigationController {
             //ナビゲーションスタックの一番上の画面（FavoriteViewController）をポップして戻る
             navigationController.popViewController(animated: true)
@@ -74,7 +73,8 @@ extension FavoreteViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return courseArray.count
     }
-    // アコーディオン機能を反映させるためisShownプロパティに基づいて
+    
+    //アコーディオン機能を反映させるためisShownプロパティに基づいて
     // 表示する行数を決定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if courseArray[section].isShown {
@@ -89,7 +89,7 @@ extension FavoreteViewController: UITableViewDataSource {
         cell.textLabel?.text = courseArray[indexPath.section].cityArray[indexPath.row]
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return courseArray[section].areaName
     }
@@ -106,14 +106,14 @@ extension FavoreteViewController: UITableViewDelegate {
         headerView.textLabel?.text = courseArray[section].areaName
         return headerView
     }
-    
+
     @objc func headerTapped(sender: UITapGestureRecognizer) {
         guard let section = sender.view?.tag else { return }
-        
+
         // アコーディオンの開閉（ビューの表示・非表示）切り替え(toggle)
         // 展開状態を保持するために isShown フラグを使用
         courseArray[section].isShown.toggle()
-        
+
         // セクションのアニメーション付き更新
         tableView.beginUpdates()
         tableView.reloadSections([section], with: .automatic)
